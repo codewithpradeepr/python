c58efc5d42e5498f26b1cf529aeb95097461b12d
pradeep <pradeeprdeveloper@gmail.com>
2023-08-30 20:21:10 +0530

    Pushing 2 folders

---
+++ Zero To Master/datatypes.py
@@ -0,0 +1,35 @@
+# Fundamental datatypes
+
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+
+print("****************")
+
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+
+print("****************")
+
+print(2 ** 2)  # Indicates power of
+print(5 // 4)  # Indicates double division
+print(5 % 4)  # Indicates reminder of
+
+print("****************")
+
+print(round(1.51))
+print(abs(-21))
+
+print("****************")
+
+# operator precedence
+
+# ()
+# **
+# * and /
+# + and -
+
+print((20-3)+2**2)
